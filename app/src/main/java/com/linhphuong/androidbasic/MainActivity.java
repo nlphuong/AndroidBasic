238131b28dd3ef7971a1cd0cce788ea89f1e3f45
nlphuong <nlphuong95@gmail.com>
2019-10-25 11:40:09 +0700

    feat: session 8

--- app/src/main/java/com/linhphuong/androidbasic/MainActivity.java
+++ app/src/main/java/com/linhphuong/androidbasic/MainActivity.java
@@ -5,20 +5,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Map;
+
 import static java.lang.Math.sqrt;
 
 public class MainActivity extends AppCompatActivity {
     int chan, le, du;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inSoChan();
-        inSoLe();
-        inSoChiaBaDuMot();
-        inChinhPhuong();
-        inNguyenTo();
-    }
+//        inSoChan();
+//        inSoLe();
+//        inSoChiaBaDuMot();
+//        inChinhPhuong();
+//        inNguyenTo();
+//      2/Lam tron len
+//        float number1 = 2.1f;
+//        number1 = (float) Math.ceil(number1);
+//        Log.d("BBB",number1+"");
+//       3/ Lam tron xuong
+//        float number1 = 2.9f;
+//        number1 = (float) Math.floor(number1);
+//        Log.d("BBB",number1+"");
+
+//       4/ Random:
+//        double number1 =  Math.floor(Math.random()*5);
+//        Log.d("BBB",number1+"");
+//        5/Tim so max, min
+//        Integer[] numbers = new Integer[]{1,10,2,5,20,18,19,21,6};
+//
+//        int i =0;
+//        int max = 0;
+//        int min = numbers[0];
+//        cach 1
+//        for (;i < numbers.length-1;i++){
+//                max = Math.max(numbers[i],numbers[i+1]);
+//        }
+//        Log.d("BBB",max +"");
+
+//        cach 2
+//        for(int value : numbers){
+//            max= Math.max(max,value);
+//            min=Math.min(min,value);
+//
+//        }
+//        Log.d("BBB",max +"");
+//        Log.d("BBB",min +"");
+
+//        7/Map
+//              + Thay doi phan tu trong mang
+//              + Tao ra 1 mang moi voi cac chuc nang duoc thay doi
+
+//        int[] arrayNuber = {1,10,11,20,18,19,21,99};
+//
+//        int[] newArray = myMap(new Map() {
+//            @Override
+//            public int onChangeValue(int value) {
+//                return (int) Math.sqrt(value);
+//            }
+//        }, arrayNuber);
+//
+//        for (int i = 0 ; i < newArray.length ; i++){
+//            Log.d("BBB",newArray[i] + "");
+//        }
+
+
+
+
+
+        }
+
 
     private void inNguyenTo() {
             for(int i =2; i<= 100;i++){
